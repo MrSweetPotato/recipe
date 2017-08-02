c5d5a63c395c2e39f8c20d328f839fbfa4361980
Timdion <503029974@qq.com>
2017-08-02 20:03:29 +0800

    BlockingQueue Test

--- MultiThread/ThreadSafeQueue/ThreadSafeQueue.cpp
+++ MultiThread/ThreadSafeQueue/ThreadSafeQueue.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <future>
+#include <memory>
 #include "BlockingQueue.h"
+
+void BlockingQueueTest(void)
+{
+	std::shared_ptr<CBlockingQueue<int>> intQueuePtr(new CBlockingQueue<int>);
+	std::async([intQueuePtr](){
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		intQueuePtr->Stop();
+	});
+	auto data = intQueuePtr->Take();
+	if (data){
+		std::cout << "take from queue:" << *data << std::endl;
+	}
+	std::cout << "BlockingQueue Stop!" << std::endl;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
-	CBlockingQueue<int> intQueue;
+	BlockingQueueTest();
+	system("pause");
 	return 0;
 }
 
