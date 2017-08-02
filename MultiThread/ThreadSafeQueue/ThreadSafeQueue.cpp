f5ff1eb55e2e8515bdeac0b506c831fc4bb42fef
Timdion <503029974@qq.com>
2017-08-02 17:38:04 +0800

    add Project BlockingQueue

---
+++ MultiThread/ThreadSafeQueue/ThreadSafeQueue.cpp
@@ -0,0 +1,11 @@
+// ThreadSafeQueue.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "BlockingQueue.h"
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CBlockingQueue<int> intQueue;
+	return 0;
+}
+
---
+++ MultiThread/ThreadSafeQueue/threadsafe_queue/BlockingQueue.h
@@ -0,0 +1,75 @@
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <memory>
+#include "nocopyable.h"
+
+#ifndef THREADSAFE_QUEUE_H_
+#define THREADSAFE_QUEUE_H_
+
+template<typename T>
+class CBlockingQueue : public CNocopyable
+{
+public:
+	CBlockingQueue() :m_StopFlag(false){}
+	virtual ~CBlockingQueue(){ Stop(); }
+
+	void Put(const T &t)
+	{
+		std::lock_guard<std::mutex> lock(m_Mutex);
+		m_Queue.push_back(t);
+		m_NotEmpty.notify_one();
+	}
+
+	void Put(T &&t)
+	{
+		std::lock_guard<std::mutex> lock(m_Mutex);
+		m_Queue.push_back(std::forward<T>(t));
+		m_NotEmpty.notify_one();
+	}
+
+	void Take(T &t)
+	{
+		std::unique_lock<std::mutex> lock(m_Mutex);
+		m_NotEmpty.wait(lock, [this](){return !m_Queue.empty(); });
+		if (!IsNeedStop()){
+			t = m_Queue.front();
+			m_Queue.pop_front();
+		}
+	}
+
+	std::shared_ptr<T> Take(void)
+	{
+		std::shared_ptr<T> tElementPtr;
+		std::unique_lock<std::mutex> lock(m_Mutex);
+		m_NotEmpty.wait(lock, [this](){return !m_Queue.empty(); });
+		if (!IsNeedStop()){
+			tElementPtr = std::make_shared<T>(m_Queue.front());
+			m_Queue.pop_front();
+		}
+		return tElementPtr;
+	}
+	
+	void Stop(void)
+	{
+		if (!IsNeedStop()){
+			StopOnce();
+		}
+	}
+
+private:
+	bool IsNeedStop(void) const { return m_StopFlag; }
+
+	void StopOnce(void)
+	{
+		m_StopFlag = true;
+		m_NotEmpty.notify_all();
+	}
+
+	std::condition_variable m_NotEmpty;
+	std::mutex				m_Mutex;
+	std::deque<T>			m_Queue;
+	bool					m_StopFlag;
+};
+
+#endif//THREADSAFE_QUEUE_H_
