88b3e7fe4c880aba6135721db33748d25d419a2e
Timdion <503029974@qq.com>
2017-10-11 09:23:40 +0800

    add some include file

--- GeneralComponent/GeneralComponent/stdafx.h
+++ GeneralComponent/GeneralComponent/stdafx.h
@@ -13,3 +13,12 @@
 
 
 // TODO:  在此处引用程序需要的其他头文件
+#include <string>
+#include <future>
+#include "any.h"
+#include "BlockingQueue.h"
+#include "BoundBlockingQueue.h"
+#include "CountDownLatch.h"
+#include "threadpool.h"
+#include "WeakCallback.h"
+
